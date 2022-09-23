6761a08bab00c38e3975eaccaf323bf84e541cfd
yusei399 <yusei2021@keio.jp>
2022-09-23 16:04:11 +0900

    migrateにreferences:user追加

--- db/migrate/20220923065401_create_user_step_logs.rb
+++ db/migrate/20220923065401_create_user_step_logs.rb
@@ -1,6 +1,7 @@
 class CreateUserStepLogs < ActiveRecord::Migration[7.0]
   def change
     create_table :user_step_logs do |t|
+      t.references :user, foreign_key: true
       t.integer :steps
       t.integer :favorite
 
