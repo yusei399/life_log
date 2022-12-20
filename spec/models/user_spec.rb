5be05a187bea7e1aca66da395e8c0969bd12b355
yusei399 <yusei2021@keio.jp>
2022-12-20 14:42:34 +0900

    spec/model追加

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+    it "is valid with a first name, last name, email, and password"
+    # 名がなければ無効な状態であること
+    it "is invalid without a first name"  
+    # 姓がなければ無効な状態であること
+    it "is invalid without a last name"
+    # メールアドレスがなければ無効な状態であること
+    it "is invalid without an email address"
+    # 重複したメールアドレスなら無効な状態であること
+    it "is invalid with a duplicate email address"
+    # ユーザーのフルネームを文字列として返すこと
+    it "returns a user's full name as a string"
+  end
+end
