86dc52c8fd42093297afbc2a30778b84e61e9c46
yusei399 <yusei2021@keio.jp>
2022-12-23 14:18:02 +0900

    factorybotPR

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,4 +20,22 @@ RSpec.describe User, type: :model do
     it "has a valid factory" do
       expect(FactoryBot.build(:user)).to be_valid
     end
+
+    it "is invalid without a first name" do
+      user = FactoryBot.build(:user, first_name: nil)
+      user.valid?
+      expect(user.errors[:first_name]).to include("can't be blank")
+    end
+
+    it "is invalid without a last name" do
+      user = FactoryBot.build(:user, last_name: nil)
+      user.valid?
+      expect(user.errors[:last_name]).to include("can't be blank")
+    end
+
+    it "is invalid without an email address" do
+      user = FactoryBot.build(:user, email: nil)
+      user.valid?
+      expect(user.errors[:email]).to include("can't be blank")
+    end
 end
