65932c9b4c6efaacd136b5d5733e4f642d267a0c
yusei399 <yusei2021@keio.jp>
2022-12-28 14:32:49 +0900

    request_specPR

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 
 config.include Devise::Test::ControllerHelpers, type: :controller
+config.include RequestSpecHelper, type: :request
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
